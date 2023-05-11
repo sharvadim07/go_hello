d4f2a23fad3461dbbae3a131489e6f9498a14d70
Vadim <sharvadim07@yandex.ru>
2023-05-11 19:00:53 +0700

    first commit

---
+++ greetings_mod/greetings.go
@@ -0,0 +1,26 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v"
+	}
+}
---
+++ hello_mod/go.mod
@@ -0,0 +1,7 @@
+module example/hello
+
+go 1.20
+
+replace example/greetings => ../greetings_mod
+
+require example/greetings v0.0.0-00010101000000-000000000000
